778cf7af337f8fe6fc9fff950d87bb263415d7dd
miyabi <miyabi04299@gmail.com>
2021-02-09 12:48:01 +0900

    Scannerクラス実装

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,15 @@
 package main;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 	System.out.println("/////////////////////////////////");
 	System.out.println("メニュー");
+	System.out.println("/////////////////////////////////");
+	int a = scan.nextInt();
+    System.out.println(a);
 	}
 
 }
