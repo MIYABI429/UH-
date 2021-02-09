deca8e78d4b4c71c95e27201d4af8a8a14c6bd90
miyabi <miyabi04299@gmail.com>
2021-02-09 14:10:14 +0900

    メニュー実装

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,12 +4,19 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 	System.out.println("/////////////////////////////////");
 	System.out.println("メニュー");
 	System.out.println("/////////////////////////////////");
-	int a = scan.nextInt();
+	System.out.println("足算1");
+	System.out.println("引算2");
+	System.out.println("割算3");
+	System.out.println("終了4");
+	System.out.println("何番の処理を行いますか");
+	int a = scanner.nextInt();
+    int b = scanner.nextInt();
     System.out.println(a);
+    System.out.println(b);
 	}
 
 }
