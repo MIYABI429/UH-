1340bba62298114e0d99ba4cf44c30967fcda41c
miyabi <miyabi04299@gmail.com>
2021-02-14 23:21:03 +0900

    リファクタリング完了

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
+//		終了しますか?の処理
 		while(true) {
+
 //	↓メニューの実装↓
 	System.out.println("/////////////////////////////////");
 	System.out.println("メニュー");
@@ -18,36 +20,37 @@ public class Main {
 	System.out.println("何番の処理を行いますか?");
 //	↑メニューの実装↑
 //  標準入力の実装
+//	↓標準入力の処理
 	Scanner scanner = new Scanner(System.in);
 	int a = scanner.nextInt();
-
+//  ↑
 	switch (a) {
 	case 1:
 //		↓足算の処理
 		System.out.println("データ１=>");
-		int d = scanner.nextInt();
-		System.out.println("データ１=>" + d);
+		int plus0 = scanner.nextInt();
+		System.out.println("データ１=>" + plus0);
 		System.out.println("データ2=>");
-		int e = scanner.nextInt();
-		System.out.println("データ2=>" + e);
-		int sum1 = d + e;
-		System.out.println("データ１=>" + d);
-		System.out.println("データ2=>" + e);
-		System.out.println("答え=>" + sum1);
+		int plus1 = scanner.nextInt();
+		System.out.println("データ2=>" + plus1);
+		int sum0 = plus0 +  plus1;
+		System.out.println("データ１=>" + plus0);
+		System.out.println("データ2=>" + plus1);
+		System.out.println("答え=>" + sum0);
 		break;
 //		↑足算の処理
 	case 2:
 //	　	↓引き算の処理
 		System.out.println("データ１=>");
-		int d1 = scanner.nextInt();
-		System.out.println("データ１=>" + d1);
+		int subtraction0 = scanner.nextInt();
+		System.out.println("データ１=>" + subtraction0);
 		System.out.println("データ2=>");
-		int e1 = scanner.nextInt();
-		System.out.println("データ2=>" + e1);
-		int sum11 = d1 - e1;
-		System.out.println("データ１=>" + d1);
-		System.out.println("データ2=>" + e1);
-		System.out.println("答え=>" +  sum11);
+		int subtraction1 = scanner.nextInt();
+		System.out.println("データ2=>" + subtraction1);
+		int sum1 = subtraction0 - subtraction1;
+		System.out.println("データ１=>" + subtraction0);
+		System.out.println("データ2=>" + subtraction1);
+		System.out.println("答え=>" +  sum1);
 		break;
 //		↑引算の処理
 	case 3:
@@ -58,24 +61,24 @@ public class Main {
 		System.out.println("データ2=>");
 		int e11 = scanner.nextInt();
 		System.out.println("データ2=>" + e11);
-		int sum111 = d11 * e11;
+		int sum2 = d11 * e11;
 		System.out.println("データ１=>" + d11);
 		System.out.println("データ2=>" + e11);
-		System.out.println("答え=>" + sum111);
+		System.out.println("答え=>" + sum2);
 		break;
 //		↑掛算の処理
 	case 4:
 //		↓割算の処理
 		System.out.println("データ１=>");
-		int d111 = scanner.nextInt();
-		System.out.println("データ１=>" + d111);
+		int division0 = scanner.nextInt();
+		System.out.println("データ１=>" + division0);
 		System.out.println("データ2=>");
-		int e111 = scanner.nextInt();
-		System.out.println("データ2=>" + e111);
-		int sum1111 = d111 / e111;
-		System.out.println("データ１=>" + d111);
-		System.out.println("データ2=>" + e111);
-		System.out.println("答え=>" + sum1111);
+		int division1 = scanner.nextInt();
+		System.out.println("データ2=>" + division0);
+		int sum3 = division0 / division0;
+		System.out.println("データ１=>" + division0);
+		System.out.println("データ2=>" + division1);
+		System.out.println("答え=>" + sum3);
 		break;
 //		↑割算の処理
 	case 99:
@@ -91,12 +94,15 @@ public class Main {
 //  ↑終了の表示
 
 
- //
+//  はい：0　いいえ：１　の標準入力の処理
      int cc = scanner.nextInt();
+//     ↓はい：０の処理
   			if(cc == 0) {
   				break;
+//  　　　↑はい：０の処理
   			}
   		}
+//		プログラムを終了しましたの処理
 	System.out.println("プログラムを終了しました。");
 
 	}
