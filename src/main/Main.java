bb6724219e66127879a4b1a818281cdf7de6717a
miyabi <miyabi04299@gmail.com>
2021-02-14 17:14:22 +0900

    繰り返し処理の実装

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,13 +10,65 @@ public class Main {
 	System.out.println("/////////////////////////////////");
 	System.out.println("足算1");
 	System.out.println("引算2");
-	System.out.println("割算3");
-	System.out.println("終了4");
-	System.out.println("何番の処理を行いますか");
+	System.out.println("掛算3");
+	System.out.println("割算4");
+	System.out.println("終了99");
+	System.out.println("/////////////////////////////////");
+	System.out.println("何番の処理を行いますか?");
+	while(true) {
 	int a = scanner.nextInt();
-    int b = scanner.nextInt();
-    System.out.println(a);
-    System.out.println(b);
+
+	switch (a) {
+	case 1:
+		int d = scanner.nextInt();
+		System.out.println("データ１=>" + d);
+		int b = scanner.nextInt();
+		System.out.println("データ2=>" + b);
+		int c = scanner.nextInt();
+		int sum = b + c;
+		System.out.println("答え=>" + sum);
+		break;
+	case 2:
+		int c1 = scanner.nextInt();
+		System.out.println("データ１=>" + c1);
+		int e = scanner.nextInt();
+		System.out.println("データ2=>" + e);
+		int sum1 = c1 - e;
+		System.out.println("答え=>" + sum1);
+		break;
+	case 3:
+		int d1 = scanner.nextInt();
+		System.out.println("データ１=>" + d1);
+		int e1 = scanner.nextInt();
+		System.out.println("データ2=>" + e1);
+		int sum11 = d1 * e1;
+		System.out.println("答え=>" + sum11);
+		break;
+	case 4:
+		int d11 = scanner.nextInt();
+		System.out.println("データ１=>" + d11);
+		int e11 = scanner.nextInt();
+		System.out.println("データ2=>" + e11);
+		int sum111 = d11 / e11;
+		System.out.println("答え=>" + sum111);
+		break;
+	case 99:
+		System.out.println("プログラムを終了しました。");
+		break;
+	default:
+		System.out.println("エラー");
+	}
+     System.out.println("終了しますか？");
+     int cc = scanner.nextInt();
+
+
+  // 6の目が出たら whileループから抜ける
+  			if(cc == 0) {
+  				break;
+  			}
+  		}
+	System.out.println("プリントを終了しました。");
+
 	}
 
 }
